44aa6ab403c34d9ac6bb302e848856f15771abcb
Tomas Havlicek <514531@mail.muni.cz>
2024-03-14 16:17:45 +0100

    Created inital template android app

---
+++ app/src/main/java/cz/muni/fi/pv239/moneytrackerproject/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package cz.muni.fi.pv239.moneytrackerproject.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/cz/muni/fi/pv239/moneytrackerproject/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package cz.muni.fi.pv239.moneytrackerproject.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
